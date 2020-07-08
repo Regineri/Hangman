5b569862312174e2ef213b7d5b286af504320361
Regineri <64798237+Regineri@users.noreply.github.com>
2020-07-08 19:44:30 +0300

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,23 +1,23 @@
 from random import *
 
-def englishDict():    #reading the text file to a list
+def englishDict(): #reading the text file to a list
     s=open("englishWords.txt","r",encoding="utf-8")
     dictionary=s.read().split()
     s.close()
     return dictionary
 
-def word_selection(dict): #selecting random word from the list dictionary
+def word_selection(dict):  #selecting random word from the list dictionary
     num=randint(0,len(dict))
     return dict[num]
 
-def list_to_str(lst): #from list to string
+def list_to_str(lst):  #makes a list of elements a string
     s=''
     for x in range(len(lst)):
         s+=lst[x]
 
     return s
 
-def check(word,char,dis): #checks if char exits in word and modifies dis if so 
+def check(word,char,dis): #checks if char exits in word and modifies dis if so
     lst=[]
     for i in range(len(word)):
         if(word[i]==char):
@@ -37,13 +37,20 @@ def add_display(dis,lst,char):
 
     return dis
 
-def print_list(used):
+def print_list(used): #prints used letters 
     s=''
     for x in range(len(used)):
         s+="{},".format(used[x])
     print("Used letters:{}\n".format(s))
 
-def play(word_bot): #main function that lets you play the game
+def check_in_alphabet(char): #checks if char is not in the english alphabet 
+    alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    if(len(char)==1 and char in alphabet):
+        return 1
+    else:
+        return 0
+
+def play(word_bot):  #main function that lets you play the game
     lives=7
     dis=" "
     used=[]
@@ -54,31 +61,34 @@ def play(word_bot): #main function that lets you play the game
         print("Lives:{}".format(lives))
         print_list(used)
         char=input("Guess letter:\n")
-        if(char not in used):
-            if(check(word_bot,char,dis)==0):
-                print("No {} in the hangman's word".format(char))
-                used.append(char)
-                print("Word |{}|".format(dis))
-                lives=lives-1
-            else:
-                dis=check(word_bot,char,dis)
-                print("Found a letter!")
-                print("Word: |{}|".format(dis))
-                used.append(char)
+        if(check_in_alphabet(char)):
+            if(char not in used):
+                if(check(word_bot, char, dis) == 0):
+                    print("No {} in the hangman's word".format(char))
+                    used.append(char)
+                    print("Word |{}|".format(dis))
+                    lives = lives - 1
+                else:
+                    dis = check(word_bot, char, dis)
+                    print("Found a letter!")
+                    print("Word: |{}|".format(dis))
+                    used.append(char)
 
-            if(dis==word_bot and lives!=0):
-                print("End of game word is |{}|".format(dis))
-                break
-            elif(lives==0):
-                print("\nHangman died. Word was:{}".format(word_bot))
-                break
-            else :
+                if (dis == word_bot and lives != 0):
+                    print("End of game word is |{}|".format(dis))
+                    break
+                elif (lives == 0):
+                    print("\nHangman died. Word was:{}".format(word_bot))
+                    break
+                else:
+                    continue
+            else:
+                print("You have used {} again".format(char))
+                print("Word:|{}|".format(dis))
                 continue
         else:
-            print("You have used {} again".format(char))
-            print("Word:|{}|".format(dis))
+            print("Letter not string or number. Spaces not allowed")
             continue
-
 
 
 dict=englishDict()
